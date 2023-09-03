3257ad6be0482e72ab756a01b9d2977bbf615c4c
BHAVAY SAVALIYA <bvysavaliya@gmail.com>
2023-09-03 16:25:55 +0530

    Update Skills.js

--- src/PortFolio/Skills/Skills.js
+++ src/PortFolio/Skills/Skills.js
@@ -29,12 +29,14 @@ function Skills() {
                         display: "flex",
                         flexWrap: "wrap",
                         justifyContent: "center",
-                        alignItems:"center"
+                        alignItems: "center"
                     }}>
-                    <SkillLabel heading={"Frontend"} list={["HTML and EJS","CSS","JavaScript","React"]} />
-                    <SkillLabel heading={"Backend & Database"} list={["Node","Express and Flask","MongoDB (with Mongoose and with Py_Mongo)"]}/>
-                    <SkillLabel heading={"Programming Languages"} list={["Java (For DSA and OOPS)","Python (For ML and DL)","C","MATLAB"]}/>
-                    <SkillLabel heading={"Machine Learning & Deep Learning"} list={["Libraries: Numpy, Pandas, Scikit-learn, keras, etc.","Neural Networks","Computer Vision"]} />
+                    <SkillLabel heading={"Programming Languages"} list={["Java (Expert)", "Python (For ML and DL)", "C (Moderate)", "MATLAB"]} />
+                    <SkillLabel heading={"Problem Solving Skills"} list={["DSA (Java)", "OOPS (Java)", "System Design (Basic)"]} />
+                    <SkillLabel heading={"Frontend"} list={["HTML and EJS", "CSS", "JavaScript", "React (Moderate)"]} />
+                    <SkillLabel heading={"Backend & Database"} list={["Node", "Express and Flask", "MongoDB (with Mongoose and with Py_Mongo) & MySQL"]} />
+                    <SkillLabel heading={"Machine Learning & Deep Learning"} list={["Libraries: Numpy, Pandas, Scikit-learn, keras, etc.", "Neural Networks", "Computer Vision"]} />
+                    <SkillLabel heading={"CS Fundamentals"} list={["Operating Systems", "Computer Networks", "DBMS"]} />
                 </div>
             </div>
         </div>
